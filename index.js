01534123459d779b9f239a2bb7a0725841a0511e
Nivetha <olajideokepelumi2002@gmail.com>
2024-07-04 16:23:23 +0100

    commit

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ app.get(`${path}hello`, async (req, res) => {
     let clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const ip = clientIp.split(",")[0].trim()
 
-
     try {
         // Using an IP geolocation service to get the location of the request.
         const geoLocation = await axios.get(`https://api.ip2location.io/?key=${process.env.IP_LOCATION_API_KEY}&ip=${ip}&format=json`);
 
-        const location = `${geoLocation.data.city_name}, ${geoLocation.data.region_name}`;
+        const location = `${geoLocation.data.region_name}`;
 
 
         //  Using a weather API to get the temperature of the requester's location
