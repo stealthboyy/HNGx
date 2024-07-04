6aeff1e5f4c4f03a06574d72ae2ad0be50c7b23e
Nivetha <olajideokepelumi2002@gmail.com>
2024-07-04 13:56:18 +0100

    commit

--- index.js
+++ index.js
@@ -13,32 +13,30 @@ app.set('trust proxy', true);
 app.get(`${path}hello`, async (req, res) => {
     const visitorName = req.query.visitor_name;
     let clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    ip = clientIp.split(",")[0].trim()
+    const ip = clientIp.split(",")[0].trim()
 
 
     try {
         // Using an IP geolocation service to get the location of the request.
-        const geoLocation = await axios.get(`https://ipinfo.io/${ip}?token=${process.env.API_KEY}`);
-        
-        const location = geoLocation.data.region;
-        console.log(location)
+        const geoLocation = await axoios.get(`https://api.ip2location.io/?key=${process.env.IP_LOCATION_API_KEY}&ip=${ip}&format=json`);
+
+        const location = `${geoLocation.data.region_name}`;
 
 
         //  Using a weather API to get the temperature of the requester's location
 
-        // const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${location}`);
+        const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${ip}`);
 
-        // const temperature = weatherResponse.data.current.temp_c;
+        const temperature = weatherResponse.data.current.temp_c;
 
         res.json({
-            // ip: clientIp,
-            // location: location,
-            // greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
+            ip: clientIp,
+            location: location,
+            greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
             });
     }
 
     catch (error){
-        console.log(error)
         res.status(500).json({ error: 'Failed to get location or the weather information'});
     }
 })
