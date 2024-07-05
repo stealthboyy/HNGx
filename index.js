79d360dc83876536afcd6a4a20d0f950a9c9f1f8
Nivetha <olajideokepelumi2002@gmail.com>
2024-07-05 13:09:00 +0100

    commit

--- index.js
+++ index.js
@@ -13,14 +13,29 @@ app.set('trust proxy', true);
 app.get(`${path}hello`, async (req, res) => {
     const visitorName = req.query.visitor_name;
     let clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const ip = clientIp.split(",")[0].trim()
+    let ip = clientIp.split(",")[0].trim()
+
+
+    const localhostVariants = [
+        "localhost", 
+        "127.0.0.1",
+        "::1",
+        "[::1]",
+        "0:0:0:0:0:0:1",
+        "::",
+    ];
+
+    if (localhostVariants.includes(ip)) {
+        ip = "127.0.0.1";
+    }
+
+
 
     try {
         // Using an IP geolocation service to get the location of the request.
         const geoLocation = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IP_LOCATION_API_KEY}&ip=${ip}`);
 
-        const location = `${geoLocation.data.state_prov}`;
-
+        const location = geoLocation.data.state_prov;
 
         //  Using a weather API to get the temperature of the requester's location
 
